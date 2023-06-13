99139650ee25b5ec65b3ec53c53b15040fb948d6
vikash533 <singhvikash533@gmail.com>
2023-06-13 12:37:32 +0530

    adding  login feature and testSteps

---
+++ OpencartBBDFramework/src/test/java/stepDefinations/steps.java
@@ -0,0 +1,44 @@
+package stepDefinations;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class steps {
+	
+	@Given("User Launch browser")
+	public void user_launch_browser() {
+	   
+	}
+
+	@Given("opens URL {string}")
+	public void opens_url(String string) {
+	    
+	}
+
+	@When("User navigate to MyAccount menu")
+	public void user_navigate_to_my_account_menu() {
+	  
+	}
+
+	@When("click on Login")
+	public void click_on_login() {
+	  
+	}
+
+	@When("User enters Email as {string} and Password as {string}")
+	public void user_enters_email_as_and_password_as(String string, String string2) {
+	  
+	}
+
+	@When("Click on Login button")
+	public void click_on_login_button() {
+	   
+	}
+
+	@Then("User navigates to MyAccount Page")
+	public void user_navigates_to_my_account_page() {
+	  
+	}
+
+}
